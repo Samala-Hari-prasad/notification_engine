b5b65db8a81ef764006359e7346346f7773ec0bd
Hari <hariprasad8587@gmail.com>
2026-02-27 23:55:45 +0530

    Reinstate DRF Browsable API renderers

--- notification_engine/settings.py
+++ notification_engine/settings.py
@@ -25,9 +25,6 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
